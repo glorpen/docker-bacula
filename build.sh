b36f2c8a6dfff420eea1ee9c96dedd0c8c035544
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-01-31 12:15:46 +0100

    make build script tag images as latest by default

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+VERSION="${1-latest}"
+
 set -ex
 
-bash generator/generate.sh client | docker build ./ -f - -t glorpen/bacula:1.0.0-client
-bash generator/generate.sh storage | docker build ./ -f - -t glorpen/bacula:1.0.0-storage
-bash generator/generate.sh console | docker build ./ -f - -t glorpen/bacula:1.0.0-console
-bash generator/generate.sh director | docker build ./ -f - -t glorpen/bacula:1.0.0-director
+bash generator/generate.sh client | docker build ./ -f - -t glorpen/bacula:${VERSION}-client
+bash generator/generate.sh storage | docker build ./ -f - -t glorpen/bacula:${VERSION}-storage
+bash generator/generate.sh console | docker build ./ -f - -t glorpen/bacula:${VERSION}-console
+bash generator/generate.sh director | docker build ./ -f - -t glorpen/bacula:${VERSION}-director
 
-bash generator/generate.sh mysql_client | docker build ./ -f - -t glorpen/bacula:1.0.0-client-mysql
+bash generator/generate.sh mysql_client | docker build ./ -f - -t glorpen/bacula:${VERSION}-client-mysql
