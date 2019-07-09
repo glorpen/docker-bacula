75cedd45b4fb65dbca1ee7cac8a9b4b74ee67373
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-07-09 10:16:31 +0200

    changed image naming convention

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ VERSION="${1-latest}"
 
 set -ex
 
-bash generator/generate.sh client | docker build ./ -f - -t glorpen/bacula:${VERSION}-client
-bash generator/generate.sh storage | docker build ./ -f - -t glorpen/bacula:${VERSION}-storage
-bash generator/generate.sh console | docker build ./ -f - -t glorpen/bacula:${VERSION}-console
-bash generator/generate.sh director | docker build ./ -f - -t glorpen/bacula:${VERSION}-director
+bash generator/generate.sh client | docker build ./ -f - -t glorpen/bacula/client:${VERSION}
+bash generator/generate.sh storage | docker build ./ -f - -t glorpen/bacula/storage:${VERSION}
+bash generator/generate.sh console | docker build ./ -f - -t glorpen/bacula/console:${VERSION}
+bash generator/generate.sh director | docker build ./ -f - -t glorpen/bacula/director:${VERSION}
 
-bash generator/generate.sh mysql_client | docker build ./ -f - -t glorpen/bacula:${VERSION}-client-mysql
+bash generator/generate.sh mysql_client | docker build ./ -f - -t glorpen/bacula/client-mysql:${VERSION}
