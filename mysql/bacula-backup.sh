6d08445b86c14f54e44249f8b8bed8bea1ecc37b
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-07-16 11:30:42 +0200

    mysql-client: check for binlogs and add flags to mysqldump

--- mysql/bacula-backup.sh
+++ mysql/bacula-backup.sh
@@ -12,8 +12,17 @@ case "$level" in
       echo "Performing full/differential backup"
       tstamp=$(date +%Y%m%d-%H%M%S)
       target="${dumpdir}/backup-${level}.${tstamp}.sql.bz2"
+
+      echo "Checking if binlogs are used"
+      has_binlogs=$(mysql --batch --silent -e 'select @@log_bin;')
+      _DUMP_ARGS=""
+      if [ "x${has_binlogs}" = "x1" ];
+      then
+         _DUMP_ARGS="${_DUMP_ARGS} --master-data=2 --delete-master-logs --flush-logs"
+      fi
+
       echo "Dumping changes to ${target}"
-      mysqldump --defaults-extra-file=/root/.my.cnf --column-statistics=0 --single-transaction --flush-logs --all-databases --routines --events --flush-privileges $BACKUP_MYSQLDUMP_ARGS | /usr/bin/pbzip2 -9 > "${target}"
+      mysqldump --defaults-extra-file=/root/.my.cnf --column-statistics=0 --single-transaction --all-databases --routines --events --flush-privileges $_DUMP_ARGS $BACKUP_MYSQLDUMP_ARGS | /usr/bin/pbzip2 -9 > "${target}"
       ;;
    "Incremental"):
       echo "Performing incremental backup"
